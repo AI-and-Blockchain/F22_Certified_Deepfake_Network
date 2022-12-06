0a539c29283460a07ed09092c612202d83ae62c8
Nathan Agpalo <nathana7221@gmail.com>
2022-12-06 11:50:42 -0500

    added model inference and reading image from bytes

--- api/app.py
+++ api/app.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 from flask import Flask, request
 from model.model import load_model
 
@@ -6,8 +9,19 @@ app = Flask(__name__)
 @app.route("/", methods=['POST'])
 def predict():
 
-    data = request.get_data()
-    print(data)
+    # Read in image
+    image = request.files['image'].read()
+    bytes = np.frombuffer(image, np.uint8)
+    img = np.array([cv2.imdecode(bytes, cv2.IMREAD_COLOR)])
+    # print(image[:100])
+    # print(bytes, len(bytes))
+    # print(img)
+
+    # Perform model inference
+    model = load_model()
+    score = model.predict(img)
+    prediction = round(score)
+    
     return "Success"
 
 if __name__ == "__main__":
